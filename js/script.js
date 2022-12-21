35218ec6f11c1f0d2e8dd474ceb0df4074986574
Iacopo Buffoni <itsmadpupp@outlook.it>
2022-12-21 10:09:05 +0100

    éush

--- js/script.js
+++ js/script.js
@@ -12,6 +12,27 @@ _Submit.addEventListener('click',
         console.log(_Kilometers.value)
         console.log(_YearsOld.value)
         
+        // Prezzo
+        let _Price = (0.21 * _Kilometers);
+
+        if (_YearsOld < 18) {
+
+            let _Discount = ((_Price / 100) * 20);
+            _Price = _Price - _Discount;
+            console.log('Hai meno di 18 anni, il prezzo originale è decurtato del 20%: ' + _Price.toFixed(2));
+
+        } else if (_YearsOld >= 65) {
+
+            let _Discount = ((_Price / 100) * 20);
+            _Price = _Price - _Discount;
+            console.log('Hai più di 65 anni, il prezzo originale è decurtato del 40%: ' + _Price.toFixed(2));
+
+        } else {
+
+            console.log('Il prezzo originale è: ' + _Price.toFixed(2));
+            
+        }
+        
     }
 )
 
